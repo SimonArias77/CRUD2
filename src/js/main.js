0819ba3896ef71067e4a25b29d8cbce39baa7cfe
SimonArias77 <sariass077@gmail.com>
2024-05-09 09:33:14 -0500

    se carga la funciòn agregar

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,53 @@ import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 import { coders } from './data.js';
-import {listWithInnerHtml} from './operations.js'
+import { index,create } from './operations.js'
 
 const tbody = document.querySelector("tbody")
-listWithInnerHtml(coders,tbody)
+const form = document.getElementById("form")
+const name = document.getElementById("name")
+const lastName = document.getElementById("last-name")
+const email = document.getElementById("email")
+const btnSave = document.getElementById("btn-save")
+
+index(coders, tbody)
+
+form.addEventListener('submit', function (event) {
+    create(coders, name, lastName, email)
+    form.reset ()
+    event.preventDefault()
+    index(coders, tbody)
+})
+
+console.log(coders)
+
+// coders.forEach(coder => {
+//     if (coder.id == 2) {
+//         coders.splice(coders.indexOf(coder), 1)
+//     }
+// })
+
+// coders = coders.filter (coder => coder.id != 2)
 
 
+
+
+// document.addEventListener("click", function () {
+//     console.log(name.value)
+//     console.log(lastName.value)
+//     console.log(email.value)
+
+//     const tempCoder = {
+//         id: new Date.now(), //para generar fecha actual con minutos y segundos
+//         name: name.value,
+//         lastName: lastName.value,
+//         email: email.value
+//     }
+
+//     coders.push(tempCoder)
+//     index(coders, tbody)
+
+// })
 
 
 
@@ -16,11 +57,11 @@ listWithInnerHtml(coders,tbody)
 
 // coders.forEach(coder => {
 //     tbody.innerHTML += `
-    // <tr>
-    //     <th scope="row">${coder.id}</th>
-    //     <td>${coder.name}</td>
-    //     <td>${coder.lastName}</td>
-    //     <td>${coder.email}</td>
-    // </tr>  
+// <tr>
+//     <th scope="row">${coder.id}</th>
+//     <td>${coder.name}</td>
+//     <td>${coder.lastName}</td>
+//     <td>${coder.email}</td>
+// </tr>
 //     `
 // });
--- src/js/operations.js
+++ src/js/operations.js
@@ -1,36 +1,69 @@
-export function listWithInnerHtml(coders,tbody) {
+export function index(coders, tbody) {
+    tbody.innerHTML = ``
     coders.forEach(coder => {
         tbody.innerHTML += `
-        <tr>
-        <th scope="row">${coder.id}</th>
-        <td>${coder.name}</td>
-        <td>${coder.lastName}</td>
-        <td>${coder.email}</td>
-    </tr>  
+            <tr> 
+                <th scope="row"> ${coder.id}</th>
+                <td> ${coder.name} </td>
+                <td> ${coder.lastName} </td>
+                <td> ${coder.email} </td>
+                <td>
+                    <button type="button" data-id="${coder.id}" class="btn btn-warning">details</button>
+                    <button type="button" data-id="${coder.id}" class="btn btn-primary">edit</button>
+                    <button type="button" data-id="${coder.id}" class="btn btn-danger">delete</button>
+                </td>
+            </tr>
         `
-    })
+    });
+}
+
+export function create(coders, name, lastName, email) {
+    const tempCoder = {
+        id: Date.now(),
+        name: name.value,
+        lastName: lastName.value,
+        email: email.value
+    }
+    coders.push(tempCoder)
 }
 
 
-function listWithInnerHtml1() {
-    coders.forEach(coder => {
-        const tr = document.createElement("tr")
-        for (let i= 0; i < 4; i++) {
-            const td = document.createElement("td")
-            tr.appendChild(td)
-        }
 
-        // tr.firstChild.textContent=coder.id
-        // tr.firstElementChild.nextElementSibling.textContent=coder.name
-        // tr.firstElementChild.nextElementSibling.nextElementSiblingtextContent=coder.lastName
-        // tr.lastElementChild.textContent=coder.email 
 
-        tr.children[0].textContent = coder.id
-        tr.children[1].textContent = coder.name
-        tr.children[2].textContent = coder.lastName
-        tr.children[3].textContent = coder.email
 
-        tbody.appendChild(tr)
 
-    })
-}
+//     coders.forEach(coder => {
+//         tbody.innerHTML += `
+//         <tr>
+//         <th scope="row">${coder.id}</th>
+//         <td>${coder.name}</td>
+//         <td>${coder.lastName}</td>
+//         <td>${coder.email}</td>
+//     </tr>  
+//         `
+//     })
+// }
+
+
+// function index1() {
+//     coders.forEach(coder => {
+//         const tr = document.createElement("tr")
+//         for (let i= 0; i < 4; i++) {
+//             const td = document.createElement("td")
+//             tr.appendChild(td)
+//         }
+
+//         // tr.firstChild.textContent=coder.id
+//         // tr.firstElementChild.nextElementSibling.textContent=coder.name
+//         // tr.firstElementChild.nextElementSibling.nextElementSiblingtextContent=coder.lastName
+//         // tr.lastElementChild.textContent=coder.email 
+
+//         tr.children[0].textContent = coder.id
+//         tr.children[1].textContent = coder.name
+//         tr.children[2].textContent = coder.lastName
+//         tr.children[3].textContent = coder.email
+
+//         tbody.appendChild(tr)
+
+//     })
+// }
